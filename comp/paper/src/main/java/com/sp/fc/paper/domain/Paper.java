a10accb9692f0fe3c866166c801fb78a573ade53
Inderby <youk8253@gmail.com>
2023-03-02 15:48:55 +0900

    import java.util.map

--- comp/paper/src/main/java/com/sp/fc/paper/domain/Paper.java
+++ comp/paper/src/main/java/com/sp/fc/paper/domain/Paper.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
